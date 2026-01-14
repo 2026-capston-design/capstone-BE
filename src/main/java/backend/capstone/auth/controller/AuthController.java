70150bc56f4e062873700cb715364e71710c67e3
zie-ning <jeeun03@gmail.com>
2026-01-14 14:45:41 +0900

    feat: 카카오 로그인 컨트롤러 구현

---
+++ src/main/java/backend/capstone/auth/controller/AuthController.java
@@ -0,0 +1,28 @@
+package backend.capstone.auth.controller;
+
+import backend.capstone.auth.dto.KakaoLoginRequest;
+import backend.capstone.auth.dto.LoginResponse;
+import backend.capstone.auth.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+	private final AuthService authService;
+
+	@PostMapping("/login/kakao")
+	@ResponseStatus(HttpStatus.CREATED)
+	public LoginResponse kakaoLogin(@RequestBody KakaoLoginRequest request) {
+		return authService.kakaoLogin(request.kakaoAccessToken());
+	}
+
+
+}
